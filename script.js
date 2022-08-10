18f687e3c595c289ba7fbd4a743aee9825e582c6
Woo Han <hanwj100@gmail.com>
2022-08-10 10:29:34 -0700

    Create working colorIn function

--- script.js
+++ script.js
@@ -6,6 +6,16 @@ function createGrid() {
     for (let i = 0; i < 256; i++) {
         const cell = document.createElement("div");
         cell.className = "cell";
+        cell.id = `cell${i}`
+        cell.addEventListener('mouseover', () => colorIn(cell.id));
         grid.appendChild(cell);
     }
 }
+
+function colorIn(cellID) {
+    const cell = document.querySelector("#" + cellID);
+    if (cell.style.backgroundColor == "") {
+        cell.style.backgroundColor = "black";
+    }
+}
+
