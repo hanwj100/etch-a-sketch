d9052328a43dda800eedb2f6d08a4358dcbe9fa4
Woo Han <hanwj100@gmail.com>
2022-08-10 11:59:07 -0700

    Create working resizeGrid func called by button
    Set default value for createGrid function.

--- script.js
+++ script.js
@@ -2,18 +2,18 @@ createResizeButton();
 createGrid();
 
 
-function createGrid() {
+function createGrid(numSides = 16) {
     const grid = document.createElement("div");
     grid.id = "grid";
 
     //grid styling
     grid.style.display = "grid";
-    grid.style.gridTemplateColumns = `repeat(16, 1fr)`;
+    grid.style.gridTemplateColumns = `repeat(${numSides}, 1fr)`;
     grid.style.width = "40vw";
     grid.style.height = "40vw";
 
     //cell creation
-    for (let i = 0; i < 256; i++) {
+    for (let i = 0; i < numSides**2; i++) {
         const cell = document.createElement("div");
         cell.className = "cell";
         cell.id = `cell${i}`
@@ -45,8 +45,26 @@ function createResizeButton() {
     const btn = document.createElement("button");
     btn.id = "resizeBtn"
     btn.textContent = "Resize";
+    btn.addEventListener("click", () => resizeGrid());
 
     const body = document.querySelector("body");
     body.appendChild(btn);
 
+}
+
+function resizeGrid() {
+    let numSides = + prompt("Please enter the number of squares per side: \n" +
+                            "(i.e. Enter 20 for a 20x20 grid. Maximum 100.)");
+    if(typeof numSides !== "number" || numSides < 0 || numSides > 100) {
+        alert("Please enter a valid number.");
+        resizeGrid();
+    }
+    else{
+        let grid = document.querySelector("#grid");
+        let body = document.querySelector("body");
+        
+        body.removeChild(grid);
+        createGrid(numSides);
+    }
+    
 }
