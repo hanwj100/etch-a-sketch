a976b0140b452973ea0d98a9014a1d681e4545d9
Woo Han <hanwj100@gmail.com>
2022-08-10 11:31:10 -0700

    Refactor css styling into js file using DOM
    Again, with the primary objective of this project being to practice
    DOM manipulation, I removed the css file and did the styling inside
    the js file.

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
 createResizeButton();
 createGrid();
 
+
 function createGrid() {
     const grid = document.createElement("div");
     grid.id = "grid";
 
+    //grid styling
+    grid.style.display = "grid";
+    grid.style.gridTemplateColumns = `repeat(16, 1fr)`;
+    grid.style.width = "40vw";
+    grid.style.height = "40vw";
+
+    //cell creation
     for (let i = 0; i < 256; i++) {
         const cell = document.createElement("div");
         cell.className = "cell";
         cell.id = `cell${i}`
         cell.addEventListener('mouseover', () => colorInCell(cell.id));
+
+        //cell styling
+        cell.style.border = "solid";
+        cell.style.padding = "10px";
+
         grid.appendChild(cell);
     }
+    
 
     const body = document.querySelector("body");
     body.appendChild(grid);
 }
+
 
 //colors in a selected cell
 function colorInCell(cellID) {
@@ -24,6 +39,7 @@ function colorInCell(cellID) {
         cell.style.backgroundColor = "black";
     }
 }
+
 
 function createResizeButton() {
     const btn = document.createElement("button");
