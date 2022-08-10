eadab25fc05d33010beaef983ed5bef0968a744a
Woo Han <hanwj100@gmail.com>
2022-08-10 11:09:25 -0700

    Create resize button function and simplify html
    This project is intended to practice using the DOM so I made the
    decision to remove the div inside the html and leave the body entirely
    blank, and instead create all necessary elements using DOM manipulation.

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+createButton();
 createGrid();
 
 function createGrid() {
-    const grid = document.querySelector("#grid");
+    const grid = document.createElement("div");
+    grid.id = "grid";
 
     for (let i = 0; i < 256; i++) {
         const cell = document.createElement("div");
@@ -10,6 +12,9 @@ function createGrid() {
         cell.addEventListener('mouseover', () => colorIn(cell.id));
         grid.appendChild(cell);
     }
+
+    const body = document.querySelector("body");
+    body.appendChild(grid);
 }
 
 //colors in a selected cell
@@ -18,4 +23,14 @@ function colorIn(cellID) {
     if (cell.style.backgroundColor == "") {
         cell.style.backgroundColor = "black";
     }
+}
+
+function createResizeButton() {
+    const btn = document.createElement("button");
+    btn.id = "resizeBtn"
+    btn.textContent = "Resize";
+
+    const body = document.querySelector("body");
+    body.appendChild(btn);
+
 }
